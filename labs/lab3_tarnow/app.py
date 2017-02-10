5091b670c5b0e2fe7ff72ae90d93718896c23832
jaketarnow <jtarnow@dons.usfca.edu>
2017-02-09 22:43:25 -0800

    testing dpkt

--- labs/lab3_tarnow/app.py
+++ labs/lab3_tarnow/app.py
@@ -9,10 +9,21 @@ def main():
         	text = sys.argv[1]
         if "http://" in sys.argv[2]:
         	req_url = sys.argv[2]
+    scrape_pcap(text)
 
-def scrape_pcap() {
-	
-}
+def scrape_pcap(text):
+	f = open(text)
+	pcap = dpkt.pcap.Reader(f)
+
+	for ts, buf in pcap:
+		eth = dpkt.ethernet.Ethernet(buf)
+	ip = eth.data
+	tcp = ip.data
+
+	if tcp.dport == 80 and len(tcp.data) > 0:
+		http = dpkt.http.Request(tcp.data)
+	print http.headers['user-agent']
+	return http.headers['user-agent']
 
 if __name__ == "__main__":
     main()
