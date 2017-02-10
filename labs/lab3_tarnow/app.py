ee131671fd77c00c61f2d6b7660022607b9de87a
jaketarnow <jtarnow@dons.usfca.edu>
2017-02-09 22:35:27 -0800

    working on lab3

---
+++ labs/lab3_tarnow/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+    # print command line arguments
+    for arg in sys.argv[1:3]:
+        if ".txt" in sys.argv[1]:
+        	text = sys.argv[1]
+        if "http://" in sys.argv[2]:
+        	req_url = sys.argv[2]
+
+def scrape_pcap() {
+	
+}
+
+if __name__ == "__main__":
+    main()
