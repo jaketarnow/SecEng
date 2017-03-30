d28b2d4306d8b799eaebf4bc8bc476fbc3494395
jaketarnow <jtarnow@dons.usfca.edu>
2017-03-29 23:40:48 -0700

    Alice and Bob work!

--- labs/lab6_tarnow/alice.py
+++ labs/lab6_tarnow/alice.py
@@ -2,6 +2,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, escape, make_response, jsonify
 from OpenSSL import SSL
 from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Cipher import AES
+from random import choice
+from string import lowercase
 import hashlib
 import base64
 import os
@@ -13,6 +17,8 @@ import codecs
 import bs4
 import re
 import sys
+import random
+import string
 
 app = Flask(__name__)
 
@@ -24,75 +30,73 @@ bob_pubKey = RSA.importKey(open(os.path.abspath("bobPubKey.pem"), 'r').read())
 @app.route('/')
 def main():
 	success = request.cookies.get('success')
+	print "IN MAIN!!"
+	print success
 	if success is None:
 		return render_template("send.html")
 	else:
-		if success == 'True':
+		if success:
 			return render_template("success.html")
 		else:
 			return render_template("tryagain.html")
 
 @app.route('/send', methods=["POST"])
 def send():
-	msg = request.form["message"]
-	shared_key = sharedKeyGen()
-	# encrypt with bob public key (message, shared_key)
-	json_objectInit = {"name" : str(msg), "sharedKey" : shared_key}
-	print json_objectInit
-	encrypt_init = alice_pubKey.encrypt(json.dumps(str(json_objectInit)), None)
-	decrypt_init = alice_privKey.decrypt(encrypt_init[0])
-	decrypt_init = json.loads(decrypt_init)
-	print decrypt_init
+	msg = request.form["message"]     
+	shared_key = sharedKeyGen()     
+	json_objectInit = json.dumps({"name" : str(msg), "sharedKey" : shared_key})
+	encrypt_init = bob_pubKey.encrypt(json_objectInit, None)
+	# decrypt_init = alice_privKey.decrypt(encrypt_init[0])
+	# decrypt_init = json.loads(decrypt_init)
+	# print decrypt_init
 
-	# url = 'http://0.0.0.0:8081/bob/send'
-	# data = {'message' : base64.b64encode(encrypt_init[0])}
-	# headers = {'Content-type': 'application/json'}
+	url = 'http://0.0.0.0:8081/bob/send'
+	data = {'message' : base64.b64encode(encrypt_init[0])}
+	headers = {'Content-type': 'application/json'}
 
-	# try:
-	# 	uResponse = requests.post(url, data=json.dumps(data), headers=headers)
-	# except requests.ConnectionError:
-	# 	return "Connection Error"
-	# Jresponse = uResponse.text
-	# data = json.loads(Jresponse)
-	# print data
-	# encrypted_nonce = data['message']
+	try:
+		uResponse = requests.post(url, data=json.dumps(data), headers=headers)
+	except requests.ConnectionError:
+		return "Connection Error"
+	Jresponse = uResponse.text
+	data = json.loads(Jresponse)
+	print data
+	encrypted_nonce = data['message']
+	print encrypted_nonce
 
-	# if encrypted_nonce != 'False':
-	# 	new_nonceSend = decryptWithSK(shared_key, encrypted_nonce)
+	if encrypted_nonce != 'False':
+		new_nonceSend = decrypt_aes(encrypted_nonce, shared_key)
+		print new_nonceSend
+		print str(new_nonceSend)
+		signed = alice_privKey.sign(str(new_nonceSend), None)
+		json_objectInit = json.dumps({"name" : str(msg), "newNonce" : signed[0]})
+		encrypted_new_nonceSend = encrypt_aes(json_objectInit, shared_key)
 
-	# 	url = 'http://0.0.0.0:8081/bob/nonceSend'
-	# 	data = {'message' : new_nonceSend}
-	# 	headers = {'Content-type': 'application/json'}
-	# 	try:
-	# 		nResponse = requests.post(url, data=json.dumps(data), headers=headers)
-	# 	except requests.ConnectionError:
-	# 		return "Connection Error"
-	# 	finalResp = nResponse.text
-	# 	data = json.loads(finalResp)
-	# 	success = data['message']
+		url = 'http://0.0.0.0:8081/bob/nonceSend'
+		data = {'message' : encrypted_new_nonceSend}
+		headers = {'Content-type': 'application/json'}
+		try:
+			nResponse = requests.post(url, data=json.dumps(data), headers=headers)
+		except requests.ConnectionError:
+			return "Connection Error"
+		finalResp = nResponse.text
+		data = json.loads(finalResp)
+		success = data['message']
+		print success
 
-	# 	if success == 'True':
-	# 		resp = make_response(redirect(url_for("main")))
-	# 		resp.set_cookie('success', json.dumps(data['message']), max_age=30)
-	# 	else:
-	# 		resp = make_response(redirect(url_for("main")))
-	# 		resp.set_cookie('success', 'False')
-	# else:
-	# 	resp = make_response(redirect(url_for("main")))
-	# 	resp.set_cookie('success', 'False')
-	resp = make_response(redirect(url_for("main")))
+		if success:
+			print "IN FINAL 1st IF"
+			resp = make_response(redirect(url_for("main")))
+			resp.set_cookie('success', json.dumps(data['message']), max_age=30)
+		else:
+			print "IN FINAL 1st ELSE"
+			resp = make_response(redirect(url_for("main")))
+			resp.set_cookie('success', 'False')
+	else:
+		print "IN FINAL 2nd ELSE"
+		resp = make_response(redirect(url_for("main")))
+		resp.set_cookie('success', 'False')
 	return resp
-
-def decryptWithSK(shared_key, nonce):
-	decrypt_nonce = RSA.importKey(shared_key).decrypt(nonce)
-	alice_priv = getAliceKey()
-	new_msg = "Alice"
-	# now need to "sign" or encrypt with Alice's private key
-	encrypt_withAlicePriv = RSA.importKey(alice_priv).encrypt(nonce, None)
-	json_objectAlice = {"name" : new_msg, "encrypted" : encrypt_withAlicePriv}
-	encrypt_recv_nonce = RSA.importKey(shared_key).encrypt(json_objectAlice, None)
-	return encrypt_recv_nonce
-
 
 def readToTxt(keysFile):
 	f = open(os.path.abspath(keysFile), 'r')
@@ -100,8 +104,9 @@ def readToTxt(keysFile):
 	return pem
 
 def sharedKeyGen():
-	key = os.urandom(16)
-	return key
+	# create something similar to 
+	n = 16
+	return "".join(choice(lowercase) for i in range(n))
 
 def getAlicePubKey():
 	f = open('aliceKey.pem', 'r')
@@ -112,6 +117,19 @@ def getAliceKey():
 	f = open('aliceKey.pem', 'r')
 	key = RSA.importKey(f.read())
 	return key
+
+# http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+def encrypt_aes(message, passphrase):
+	IV = Random.new().read(AES.block_size)
+	aes = AES.new(passphrase, AES.MODE_CFB, IV)
+	return base64.b64encode(IV + aes.encrypt(message))
+
+# http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+def decrypt_aes(encrypted, passphrase):
+	new_encrypted = base64.b64decode(encrypted)
+	IV = new_encrypted[:AES.block_size]
+	aes = AES.new(passphrase, AES.MODE_CFB, IV)
+	return aes.decrypt(new_encrypted[AES.block_size:]).decode('utf-8')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8080, debug = True)
--- labs/lab6_tarnow/bob.py
+++ labs/lab6_tarnow/bob.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session, escape, make_response, jsonify
 from OpenSSL import SSL
 from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Cipher import AES
 import hashlib
 import base64
 import os
@@ -32,42 +34,56 @@ bob_privKey = RSA.importKey(open(os.path.abspath("bobKey.pem"), 'r').read())
 
 @app.route('/bob/send', methods=['GET', 'POST'])
 def send():
-	data = request.get_json()
-	encrypt_init = data['message']
+	global shared_key
+	global nonce
+	data = request.get_data()
+	encrypt_init = json.loads(data)
+	encrypt_init = encrypt_init['message']
 	encrypt_init = base64.b64decode(encrypt_init)
-	decrypt_init = bob_privKey.decrypt(encrypt_init)
-	print decrypt_init
-	# name = json_decrypt_init['name']
-	# shared_key = json_decrypt_init['sharedKey']
 
-	# if msg == 'Alice':
-	# 	nonce = generate_nonce()
-	# 	encrypted_nonce = RSA.importKey(shared_key).encrypt(nonce, None)
-	# 	jsonify = {'message': encrypted_nonce[0]}
-	# else:
-	jsonify = {'message': 'False'}
+	decrypt_init = bob_privKey.decrypt(encrypt_init)
+	json_decrypt_init = json.loads(decrypt_init)
+	msg = json_decrypt_init['name']
+	shared_key = json_decrypt_init['sharedKey']
+
+	if msg == 'Alice':
+		nonce = generate_nonce()
+		print nonce
+		encrypted_nonce = encrypt_aes(nonce, shared_key)
+		print encrypted_nonce
+		jsonify = {'message': encrypted_nonce}
+	else:
+		jsonify = {'message': 'False'}
 	return json.dumps(jsonify, indent=4)
 
 
 @app.route('/bob/nonceSend', methods=['GET', 'POST'])
 def nonceSend():
-	data = request.get_json()
-	encrypt_nonce = data['message']
-	decrypt_init = RSA.importKey(shared_key).decrypt(encrypt_nonce)
-	msg = decrypt_init[0]
-	alice_encrypt_none = decrypt_init[1]
+	global shared_key
+	global nonce
+	data = request.get_data()
+	encrypt_nonce = json.loads(data)
+	encrypt_nonce = encrypt_nonce['message']
+	decrypt_init = decrypt_aes(encrypt_nonce, shared_key)
+	print decrypt_init
+	decrypt_init = json.loads(decrypt_init)
+	msg = decrypt_init['name']
+	print msg
+	new_nonce = decrypt_init['newNonce']
+	print new_nonce
 
 	if msg == 'Alice':
 		alice_pubKey = getAlicePubKey()
-		decrypt_nonce = RSA.importKey(alice_pubKey).decrypt(alice_encrypt_none)
-		new_nonce = decrypt_nonce[0]
+		new_nonce = (long(new_nonce),)
+		decrypt_nonce = alice_pubKey.verify(nonce, new_nonce)
+		print decrypt_nonce
 		# if new_nonce is same as nonce that Bob created, then send success!
-		if new_nonce == nonce:
-			jsonify = {'message': 'True'}
+		if decrypt_nonce:
+			jsonify = {'message': True}
 		else:
-			jsonify = {'message': 'False'}
+			jsonify = {'message': False}
 	else:
-		jsonify = {'message': 'False'}
+		jsonify = {'message': False}
 	return json.dumps(jsonify, indent=4)
 
 def readToTxt(keysFile):
@@ -89,6 +105,19 @@ def getBobKey():
 	f = open('bobKey.pem', 'r')
 	key = RSA.importKey(f.read())
 	return key
+
+# http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+def encrypt_aes(message, passphrase):
+	IV = Random.new().read(AES.block_size)
+	aes = AES.new(passphrase, AES.MODE_CFB, IV)
+	return base64.b64encode(IV + aes.encrypt(message))
+
+# http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
+def decrypt_aes(encrypted, passphrase):
+	new_encrypted = base64.b64decode(encrypted)
+	IV = new_encrypted[:AES.block_size]
+	aes = AES.new(passphrase, AES.MODE_CFB, IV)
+	return aes.decrypt(new_encrypted[AES.block_size:]).decode('utf-8')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8081, debug = True)
