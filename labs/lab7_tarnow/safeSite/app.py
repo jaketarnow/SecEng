0fbdf005a2f2cb6ae1a2a8792f0f170bc446e7dc
jaketarnow <jtarnow@dons.usfca.edu>
2017-04-17 17:30:18 -0700

    complete lab7

---
+++ labs/lab7_tarnow/safeSite/app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for, session, escape, make_response, jsonify
+from OpenSSL import SSL
+from Crypto.PublicKey import RSA
+import hashlib
+import base64
+import os
+import Cookie
+import datetime
+import json
+import requests
+import codecs
+import bs4
+import re
+
+#context = SSL.Context(SSL.SSLv23_METHOD)
+#cer = os.path.join(os.path.dirname(__file__), 'certificate.crt')
+#key = os.path.join(os.path.dirname(__file__), 'privateKey.key')
+app = Flask(__name__)
+app.secret_key = os.urandom(24).encode('hex')
+
+@app.route('/')
+def main():
+	user_id = request.cookies.get('userID')
+
+	if user_id != None:
+		if user_id == 'nope':
+			return render_template("nameindex.html")
+		elif user_id == 'Welcome':
+			return render_template("index.html")
+	else:
+		return render_template("login.html")
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+	if request.method == "POST":
+		username_form = request.form["username"]
+		password_form = request.form["password"]
+
+		url = 'http://0.0.0.0:8081/api/login'
+		jData = {'username' : username_form, 'password' : password_form}
+		headers = {'Content-type': 'application/json'}
+		try:
+			# build request with data as json object
+			uResponse = requests.post(url, data=json.dumps(jData), headers=headers)
+		except requests.ConnectionError:
+			return "Connection Error"
+		Jresponse = uResponse.text
+		data = json.loads(Jresponse)
+
+		if data['success'] == True:
+			editHTML(data['answer'])
+			resp = make_response(redirect(url_for("main")))
+			resp.set_cookie('userID', 'Welcome', max_age=30)
+		else:
+			editHTML(data['answer'])
+			resp = make_response(redirect(url_for("main")))
+			resp.set_cookie('userID', 'Welcome', max_age=30)
+		return resp
+	return render_template("login.html")
+
+def editHTML(user_name):
+	with open("templates/index.html") as inf:
+		txt = inf.read()
+		soup = bs4.BeautifulSoup(txt)
+	# create new elem
+	var = user_name.title()
+	for i in soup.find_all(class_='test'):
+		i.string = var
+	# save file again
+	with open("templates/index.html", "w") as outf:
+		outf.write(str(soup))
+
+
+@app.route('/logout')
+def logout():
+	editHTML(" ")
+	resp = make_response(redirect(url_for("main")))
+	resp.set_cookie('userID', expires=0)
+	return resp
+
+if __name__ == "__main__":
+	#context = (cer, key)
+	#app.run(host='0.0.0.0', debug = True, ssl_context=context)
+	app.run(host='0.0.0.0', port=8080, debug = True)
---
+++ labs/lab7_tarnow/unsafeSite/dataapp.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect, url_for, session, escape, make_response
+from OpenSSL import SSL
+from Crypto.PublicKey import RSA
+import hashlib
+import base64
+import MySQLdb
+import os
+import Cookie
+import datetime
+import json
+import requests
+import urllib2
+import re
+import time
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24).encode('hex')
+
+if __name__ == "__main__":
+	db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="cs683")
+	cur = db.cursor()
+
+class ServerError(Exception):pass
+
+@app.route('/api/login', methods=['GET', 'POST'])
+def login():
+	data = request.get_json()
+	usern = data['username']
+	pwd = data['password']
+	everything = "Awesome!"
+
+	if pwd != None:
+		sql = "SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (usern, pwd)
+		print sql
+		cur.execute(sql)
+		everything = str(cur.fetchall())
+		print everything
+		if everything is not None:
+			print "here 1"
+			jsonify = {'success': True, 'answer': everything}
+		else:
+			print "here 2"
+			jsonify = {'success': False, 'answer': 'oops'}
+		return json.dumps(jsonify, indent=4)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=8081, debug = True)
